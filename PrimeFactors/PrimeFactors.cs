dad736f45f5a9eda3e054d355367c3cbc92ed94f
Turo Nylund <turo.nylund@outlook.com>
2015-07-27 20:05:59 +0300

    PrimeFactor kata:
    - tried NUnit framework.
    - have no idea how to get further than 5.
    - need to read about prime factors.

---
+++ PrimeFactors/PrimeFactors.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeFactors
+{
+    public class PrimeFactors
+    {
+        public List<int> GetPrimeFactors(int number)
+        {
+            List<int> factors = new List<int>();
+
+            if (number > 1)
+                factors.Add(number);
+
+            return factors;
+        }
+    }
+}
