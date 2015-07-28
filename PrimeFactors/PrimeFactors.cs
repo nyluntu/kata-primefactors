c5872c7987489522361b46f3c46e2dd1311b22a1
Turo Nylund <turo.nylund@outlook.com>
2015-07-28 10:47:27 +0300

    Prime factors kata solved first time.
    - tried TDD.
    - learned how TDD is pushing towards generic solution.
    - a few times it take time to understand what happened.
    - good training for first time solution but can be tricky one.

--- PrimeFactors/PrimeFactors.cs
+++ PrimeFactors/PrimeFactors.cs
@@ -12,8 +12,9 @@ namespace PrimeFactors
         {
             List<int> factors = new List<int>();
 
-            if (number > 1)
-                factors.Add(number);
+            for (int divider = 2; number > 1; divider++)
+                for (; number % divider == 0; number = number / divider)
+                    factors.Add(divider);
 
             return factors;
         }
